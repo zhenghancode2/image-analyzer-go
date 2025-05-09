0251407e9c0addbca625247129e185f4fc24d96b
zhenghancode2 <han.applemail@icloud.com>
2025-05-09 16:28:55 +0800

    fix change os info path

--- pkg/analyze/os.go
+++ pkg/analyze/os.go
@@ -8,7 +8,8 @@ import (
 )
 
 func CheckOSInfo(root string) string {
-	path := filepath.Join(root, "etc", "os-release")
+	// 只有运行的容器里/etc/os-release才会被建立软链，这里静态的直接取/usr/lib/os-release
+	path := filepath.Join(root, "usr", "lib", "os-release")
 	data, err := os.ReadFile(path)
 	if err != nil {
 		logger.Error("读取操作系统信息失败", logger.WithError(err))
